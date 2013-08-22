77e6d58aa504027bd8c12b8392dab93c56c401c4
jtuohy7 <jtuohy7@gmail.com>
2013-08-22 17:07:06 -0500

    AudioProcessor, Synthesizer and MakeFile changes

--- synthesizer/src/AudioProcessor.cpp
+++ synthesizer/src/AudioProcessor.cpp
@@ -1,10 +1,58 @@
-#include "AudioProcessor.hpp"
 
-namespace chip 
+#pragma once
+
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include "portaudio.h"
+#include "mixer.hpp"
+//#include "Module.hpp"
+
+//NUM-SECONDS is simply a playback mechanism
+#define NUM_SECONDS   (1)
+
+//Sample rate of the soundcard
+#define SAMPLE_RATE   (32000)
+
+//Audio buffer size
+#define FRAMES_PER_BUFFER  (256)
+
+//Power of wavetable size (wavetable size = 2 ^ POWER)
+#define POWER (4)
+#define TABLE_SIZE   (1<<POWER)
+
+//Pi, for generation of sine table
+#ifndef M_PI
+#define M_PI  (3.14159265)
+#endif
+
+
+namespace chip
 {
-    AudioProcessor::AudioProcessor()
+
+    class AudioProcessor
     {
-        
-    }
-    
-}
+        public:
+            AudioProcessor();
+            ~AudioProcessor();
+            
+            //void generateWavetables();
+            
+            /* This routine will be called by the PortAudio engine when audio is needed.
+            ** It may called at interrupt level on some machines so don't do anything
+            ** that could mess up the system like calling malloc() or free().
+            */
+            static int paCallback( const void *inputBuffer, void *outputBuffer,
+                                   unsigned long framesPerBuffer,
+                                   const PaStreamCallbackTimeInfo* timeInfo,
+                                   PaStreamCallbackFlags statusFlags,
+                                   void *userData );
+            
+        private:
+            Mixer masterMixer;
+            //vector<Module> modules;
+    };
+
+} 
+
