0ecf074fd2fb173cb8d7a2f4b620e888208fba13
Dustin Amesbury <ddamesbury@gmail.com>
2013-11-25 23:15:34 -0600

    started songbox application code

---
+++ songbox/src/Songbox.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "MidiFileNameReader.hpp"
+#include "juce_midistuff/juce_MidiOutput.h"
+#include "juce_midistuff/juce_MidiMessageSequence.h"
+#include "juce_midistuff/juce_MidiMessage.h"
+#include "juce_midistuff/juce_MidiFile.h"
+
+
