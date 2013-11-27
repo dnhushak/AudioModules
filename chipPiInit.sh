2fbb7484588509c551cb9465040c1c14fa616dba
Darren Hushak <dnhushak@pinnacle-recording.com>
2013-11-27 01:59:21 -0600

    Attempting to fix the bashrc echo command

--- chipPiInit.sh
+++ chipPiInit.sh
@@ -22,7 +22,21 @@ make
 sudo make install
 cd ..
 rm -r ttymidi
-cd
+cd ~/
+
+####################
+# Baud Rate Trickery
+####################
+
+sudo echo '# change uart clock to 2441406 for MIDI 31250 baud rate
+# default is init_uart_clock=3000000
+
+init_uart_clock=2441406
+init_uart_baud=38400' >> /boot/config.txt
+
+sudo echo 'dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait bcm2708.uart_clock=3000000' > /boot/cmdline.txt
+
+sudo sed -i '/T0:23:respawn:\/sbin\/getty -L ttyAMA0 115200 vt100/ s/^/# /' /etc/inittab
 
 
 
@@ -46,19 +60,3 @@ alias midithruon="aconnect 128:0 128:1"
 alias midithruoff="aconnect -d 128:0 128:1"
 startmidi
 midithruon' >> ~/.bashrc
-
-
-
-####################
-# Baud Rate Trickery
-####################
-
-sudo echo '# change uart clock to 2441406 for MIDI 31250 baud rate
-# default is init_uart_clock=3000000
-
-init_uart_clock=2441406
-init_uart_baud=38400' >> /boot/config.txt
-
-sudo echo 'dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait bcm2708.uart_clock=3000000' > /boot/cmdline.txt
-
-sudo sed -i '/T0:23:respawn:\/sbin\/getty -L ttyAMA0 115200 vt100/ s/^/# /' /etc/inittab
