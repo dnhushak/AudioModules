5168358415bb20aa70ca7d5b03486ce2dfa4114d
Darren Hushak <dnhushak@pinnacle-recording.com>
2014-01-24 21:04:34 -0600

    Fixed a few weird things with the UI arduino code. Squashed a but related to the LED's of the transport controls. Added a startup sequence

--- ui/ChipophoneUI/ChipophoneUI.ino
+++ ui/ChipophoneUI/ChipophoneUI.ino
@@ -3,7 +3,7 @@
 
 const int numModules = 5;
 
-const int moduleStartPin = 25;
+const int moduleStartPin = 2;
 
 const int midiInPin = 18;
 const int midiOutPin = 19;
@@ -56,7 +56,7 @@ const int arpeggioGrnLED = 33;
 const int arpeggioBluLED = 32;
 
 int arpeggioLEDs[] = {
-    arpeggioRedLED, arpeggioGrnLED, arpeggioBluLED };
+  arpeggioRedLED, arpeggioGrnLED, arpeggioBluLED };
 
 //int *lastArpeggioState;
 int lastArpeggioState = HIGH;
@@ -68,7 +68,7 @@ const int glissGrnLED = 30;
 const int glissBluLED = 29;
 
 int glissLEDs[] = {
-    glissRedLED, glissGrnLED, glissBluLED };
+  glissRedLED, glissGrnLED, glissBluLED };
 
 int lastGlissState = HIGH;
 boolean glissOn[numModules];
@@ -76,7 +76,7 @@ boolean glissOn[numModules];
 /******************************************************************************/
 /* Songbox Buttons and LEDs                                                                    */
 /******************************************************************************/
-/*
+
 const int playButton = A9;
 const int playLED = 26;
 
@@ -88,7 +88,7 @@ const int stopLED = 27;
 
 const int recdButton = A10;
 const int recdLED = 28;
-*/
+
 
 /******************************************************************************/
 /* Encoders                                                                   */
@@ -154,25 +154,27 @@ void sendMidi(int message, int data1, int data2) {
 }
 
 void setup() {
+
   int i;
 
-  
+
   // Select Channel to Edit
   for(i=0; i<numModules; i++)
   {
-     pinMode(channelSelectorButtons[i], INPUT);
-     digitalWrite(channelSelectorButtons[i], HIGH); // RELEASED
-     
-     pinMode(channelSelectorLEDs[i], OUTPUT);
-     digitalWrite(channelSelectorLEDs[i], HIGH); // OFF
+    pinMode(channelSelectorButtons[i], INPUT);
+    digitalWrite(channelSelectorButtons[i], HIGH); // RELEASED
+
+    pinMode(channelSelectorLEDs[i], OUTPUT);
+    digitalWrite(channelSelectorLEDs[i], HIGH); // OFF
   }
-  
+
   // Initialize the module to 0
   digitalWrite(channelSelectorLEDs[0], LOW); //on
   currentlySelectedModule = 0;
-  
+
   // Arpeggio
   pinMode(arpeggioButton, INPUT);
+  //Gives button a pullup resistor
   digitalWrite(arpeggioButton, HIGH);
   pinMode(arpeggioRedLED, OUTPUT); 
   turnOff(arpeggioRedLED);
@@ -180,15 +182,16 @@ void setup() {
   turnOff(arpeggioGrnLED);
   pinMode(arpeggioBluLED, OUTPUT);
   turnOff(arpeggioBluLED);
-  
+
   for(i = 0; i < numModules; i++)
   {
     // Initially all modules have arpeggio off
     arpeggioOn[i] = false;
   }
-  
+
   // Glissando
   pinMode(glissButton, INPUT);
+  //Gives button a pullup resistor
   digitalWrite(glissButton, HIGH);
   pinMode(glissRedLED, OUTPUT); 
   turnOff(glissRedLED);
@@ -196,59 +199,63 @@ void setup() {
   turnOff(glissGrnLED);
   pinMode(glissBluLED, OUTPUT);
   turnOff(glissBluLED);
-  
+
   for(i = 0; i < numModules; i++)
   {
     // Initially all modules have arpeggio off
     glissOn[i] = false;
   }
-/*
+
   // Songbox 
-  pinMode(buttonPlayPin, INPUT);
-  pinMode(buttonPausPin, INPUT);
-  pinMode(buttonStopPin, INPUT);
-  pinMode(buttonRecdPin, INPUT);
-  
-  pinMode(LEDPlayPin, OUTPUT);     
-  pinMode(LEDStopPin, OUTPUT);     
-  pinMode(LEDPausPin, OUTPUT);     
-  pinMode(LEDRecdPin, OUTPUT);     
-  */
+  pinMode(playButton, INPUT);
+  pinMode(pausButton, INPUT);
+  pinMode(stopButton, INPUT);
+  pinMode(recdButton, INPUT);
+
+
+  pinMode(playLED, OUTPUT);
+  pinMode(pausLED, OUTPUT);
+  pinMode(stopLED, OUTPUT);
+  pinMode(recdLED, OUTPUT);
+
   // MIDI I/O
   pinMode(midiInPin, INPUT);
   pinMode(midiOutPin, OUTPUT);
-  
+
   // Encoders
+  /*
   pinMode(encoderAChannelVolumeAPin, INPUT);
-  pinMode(encoderAChannelVolumeBPin, INPUT);
-  pinMode(encoderBArpeggioAPin, INPUT);
-  pinMode(encoderBArpeggioBPin, INPUT);
-  pinMode(encoderCGlissandoAPin, INPUT);
-  pinMode(encoderCGlissandoBPin, INPUT);
-  pinMode(encoderDTempoAPin, INPUT);
-  pinMode(encoderDTempoBPin, INPUT);
-  
-  for (i = 0; i< numModules; i++){
-      modulePin[i] = moduleStartPin + i;
-      pinMode(modulePin[i], INPUT); 
-    
-      /* Encoders */
-      encoderAPos[i] = 0;
-      pinMode(encoderAPos[i], INPUT);
-      encoderBPos[i] = 0;
-      pinMode(encoderBPos[i], INPUT);
-      encoderCPos[i] = 0;
-      pinMode(encoderCPos[i], INPUT);
-      encoderDPos[i] = 0;
-      pinMode(encoderDPos[i], INPUT);
-  }
-  
+   pinMode(encoderAChannelVolumeBPin, INPUT);
+   pinMode(encoderBArpeggioAPin, INPUT);
+   pinMode(encoderBArpeggioBPin, INPUT);
+   pinMode(encoderCGlissandoAPin, INPUT);
+   pinMode(encoderCGlissandoBPin, INPUT);
+   pinMode(encoderDTempoAPin, INPUT);
+   pinMode(encoderDTempoBPin, INPUT);
+   
+   for (i = 0; i< numModules; i++){
+   modulePin[i] = moduleStartPin + i;
+   pinMode(modulePin[i], INPUT); 
+   
+   // Encoders
+   encoderAPos[i] = 0;
+   pinMode(encoderAPos[i], INPUT);
+   encoderBPos[i] = 0;
+   pinMode(encoderBPos[i], INPUT);
+   encoderCPos[i] = 0;
+   pinMode(encoderCPos[i], INPUT);
+   encoderDPos[i] = 0;
+   pinMode(encoderDPos[i], INPUT);
+   }
+   */
+
   Serial1.begin(31250);
   Serial.begin(9600);
   Serial.println("Start");
-  
+
+  demoSequence();
+
   //sendMidi(controlChange, arpeggio, toggle);
-  
 
 }
 
@@ -257,107 +264,107 @@ void changeLEDColor(int *rgbPin, int desiredColor )
   int R = 0;
   int G = 1;
   int B = 2;
-  
+
   switch (desiredColor)
   {  
-    case RED:
-      turnOn(rgbPin[R]);
-      turnOff(rgbPin[G]);
-      turnOff(rgbPin[B]);
-      break;
-      
-    case YEL:
-      turnOn(rgbPin[R]);
-      turnOn(rgbPin[G]);
-      turnOff(rgbPin[B]);
-      break;
-      
-    case GRN:
-      turnOff(rgbPin[R]);
-      turnOn(rgbPin[G]);
-      turnOff(rgbPin[B]);
-      break;
-      
-    case BLU:
-      turnOff(rgbPin[R]);
-      turnOff(rgbPin[G]);
-      turnOn(rgbPin[B]);
-      break;
-      
-    case WHT:
-      turnOn(rgbPin[R]);
-      turnOn(rgbPin[G]);
-      turnOn(rgbPin[B]);
-      break;
-      
-    default:
-      turnOff(rgbPin[R]);
-      turnOff(rgbPin[G]);
-      turnOff(rgbPin[B]);
-    }
+  case RED:
+    turnOn(rgbPin[R]);
+    turnOff(rgbPin[G]);
+    turnOff(rgbPin[B]);
+    break;
+
+  case YEL:
+    turnOn(rgbPin[R]);
+    turnOn(rgbPin[G]);
+    turnOff(rgbPin[B]);
+    break;
+
+  case GRN:
+    turnOff(rgbPin[R]);
+    turnOn(rgbPin[G]);
+    turnOff(rgbPin[B]);
+    break;
+
+  case BLU:
+    turnOff(rgbPin[R]);
+    turnOff(rgbPin[G]);
+    turnOn(rgbPin[B]);
+    break;
+
+  case WHT:
+    turnOn(rgbPin[R]);
+    turnOn(rgbPin[G]);
+    turnOn(rgbPin[B]);
+    break;
+
+  default:
+    turnOff(rgbPin[R]);
+    turnOff(rgbPin[G]);
+    turnOff(rgbPin[B]);
+  }
 }
 
 int readEncoder(int encoderPin){
   int ret = 0;
   /*int encoderPinA = encoderPin;
-  int encoderPinB = encoderPin + 1;
-  n = digitalRead(encoderPinA);
-  if ((encoder0PinALast == LOW) && (n == HIGH)) {
-    if (digitalRead(encoderPinB) == LOW) {
-      ret = -1;
-    } 
-    else {
-      ret = 1;
-    }
-  } 
-  encoder0PinALast = n;
-  if(ret != 0){
-    Serial.print(ret);
-    Serial.print("\n");
-  }
-  */
+   int encoderPinB = encoderPin + 1;
+   n = digitalRead(encoderPinA);
+   if ((encoder0PinALast == LOW) && (n == HIGH)) {
+   if (digitalRead(encoderPinB) == LOW) {
+   ret = -1;
+   } 
+   else {
+   ret = 1;
+   }
+   } 
+   encoder0PinALast = n;
+   if(ret != 0){
+   Serial.print(ret);
+   Serial.print("\n");
+   }
+   */
   return ret;
 }
 
 
 void selectChannelToEdit()
 {
-    int i=0;
-    
-    for(i = 0; i < numModules; i++)
+  int i=0;
+
+  for(i = 0; i < numModules; i++)
+  {
+    int button = channelSelectorButtons[i];
+    int led = channelSelectorLEDs[i];
+
+    if(isPressed(button))
     {
-       int button = channelSelectorButtons[i];
-       int led = channelSelectorLEDs[i];
-       
-       if(isPressed(button))
-       {
-         turnOff(channelSelectorLEDs[currentlySelectedModule]);
-         currentlySelectedModule = i;
-         
-         // Turn on the currently selected module LED
-         turnOn(led);
-         
-         // Update the channel editting button LEDs
-         if(arpeggioOn[currentlySelectedModule])
-         {
-           changeLEDColor(arpeggioLEDs, currentlySelectedModule);
-         }  
-         else
-         {
-           changeLEDColor(arpeggioLEDs, OFF);
-         }
-         
-         if(glissOn[currentlySelectedModule])
-         {
-           changeLEDColor(glissLEDs, currentlySelectedModule);
-         }  
-         else
-         {
-           changeLEDColor(glissLEDs, OFF);
-         }
-         break;
-       }  
-    } 
+      turnOff(channelSelectorLEDs[currentlySelectedModule]);
+      currentlySelectedModule = i;
+
+      // Turn on the currently selected module LED
+      turnOn(led);
+
+      // Update the channel editting button LEDs
+      if(arpeggioOn[currentlySelectedModule])
+      {
+        changeLEDColor(arpeggioLEDs, currentlySelectedModule);
+      }  
+      else
+      {
+        changeLEDColor(arpeggioLEDs, OFF);
+      }
+
+      if(glissOn[currentlySelectedModule])
+      {
+        changeLEDColor(glissLEDs, currentlySelectedModule);
+      }  
+      else
+      {
+        changeLEDColor(glissLEDs, OFF);
+      }
+      break;
+    }  
+  } 
 }
 
 void updateChannelProps(int button, int* lastState, int* rgbPins, boolean* moduleState )
@@ -366,23 +373,23 @@ void updateChannelProps(int button, int* lastState, int* rgbPins, boolean* modul
   {
     *lastState = LOW;
     delay(5);
-    
+
     // Toggle the property
     if(moduleState[currentlySelectedModule])
     {
       // Turn all the things off
       changeLEDColor(rgbPins, OFF);
       moduleState[currentlySelectedModule] = false;
-      
+
       // TODO Send midi arpeggio OFF for current module
-       
+
     }  
     else
     {
       // Set led buttons to the color of the selected channel.
       changeLEDColor(rgbPins, currentlySelectedModule);
       moduleState[currentlySelectedModule] = true;
-      
+
       // TODO Send midi arpeggio ON for current module
     }
   }
@@ -396,11 +403,39 @@ void loop()
 {
   // Check status of  Channel Selector buttons
   selectChannelToEdit();
-  
+
   // Argeggio and glissando have similar logic
   updateChannelProps(arpeggioButton, &lastArpeggioState, arpeggioLEDs, arpeggioOn);
   updateChannelProps(glissButton, &lastGlissState, glissLEDs, glissOn);
 }
+
+void demoSequence(){
+  int numLEDs = 15;
+  int LEDs[] = {
+    redSelectorLED, yelSelectorLED, grnSelectorLED, bluSelectorLED, whtSelectorLED, arpeggioRedLED, arpeggioGrnLED, arpeggioBluLED, glissRedLED, glissGrnLED, glissBluLED, playLED, pausLED, stopLED, recdLED  };
+
+  int j;
+  int i;
+  for (j=0; j<4; j++){
+    for (i=0; i<numLEDs; i++){
+      turnOn(LEDs[i]); 
+    }
+    delay(300);
+    for (i=0; i<numLEDs; i++){
+      turnOff(LEDs[i]); 
+    }
+    delay(300);
+  }
+  for (i=0; i<numLEDs; i++){
+    turnOn(LEDs[i]);
+    delay(200);
+    turnOff(LEDs[i]);
+  }
+
+
+
+}
+
 
 
 
