fcf9ab3dcf4e2cfb8bdeeec31abfc65d0b546fbc
Darren Hushak <dnhushak@pinnacle-recording.com>
2014-03-17 02:26:05 -0500

    Mixer editing for further rPI Tests

--- synthesizer/src/Synthesizer.cpp
+++ synthesizer/src/Synthesizer.cpp
@@ -218,18 +218,14 @@ int main(int argc, char *argv[]) {
 	if (pmerr != pmNoError) {
 		exit(0);
 	}
-	modules->at(0)->activatePolyVoice(60);
-	modules->at(0)->activatePolyVoice(64);
-	modules->at(0)->activatePolyVoice(67);
-	modules->at(0)->activatePolyVoice(72);
 
-//	while (1) {
-//		// Read MIDI, forward
-//		PMHandler->readMIDI();
-//		masterMixer->cleanup();
-//	}
+	while (1) {
+		// Read MIDI, forward
+		PMHandler->readMIDI();
+		masterMixer->cleanup();
+		Pa_Sleep(15);
+	}
 
-	Pa_Sleep(1500);
 	PAHandler->disconnectAudioStream();
 	PMHandler->disconnectMIDIStream();
 
