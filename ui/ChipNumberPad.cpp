f9284162c30a32236800302096a017d039133b98
Darren Hushak <dnhushak@pinnacle-recording.com>
2014-04-03 15:16:23 -0500

    Set data2 of the message to 0; prevention of weird MIDI Data being sent

--- ui/ChipNumberPad.cpp
+++ ui/ChipNumberPad.cpp
@@ -14,6 +14,7 @@ namespace chip {
 		char keys[rows][cols] = { { '1', '2', '3' }, { '4', '5', '6' }, { '7',
 				'8', '9' }, { '*', '0', '#' } };
 		keypad = new Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
+
 		// Initialize key input info
 		selectionValue = 0;
 		key = '0';
@@ -25,6 +26,7 @@ namespace chip {
 		// Initialize MIDI
 		AMHandler = initAMHandler;
 		message = new synth::MIDIMessage;
+		message->data2 = 0;
 	}
 
 	void ChipNumberPad::poll(int state) {
