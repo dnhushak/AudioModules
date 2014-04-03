9fd33cef195cdc241aee72bdb436b2e9856cd9b6
Darren Hushak <dnhushak@pinnacle-recording.com>
2014-04-03 14:04:26 -0500

    Added blink and toggle to RGBLED

--- ui/ChipophoneUI/RGBLED.cpp
+++ ui/ChipophoneUI/RGBLED.cpp
@@ -1,30 +1,67 @@
 #include "RGBLED.h"
 
 namespace ArduinoUI {
-RGBLED::RGBLED(int redPin, int grnPin, int bluPin) {
-  redLED = new ArduinoUI::LED(redPin);
-  grnLED = new ArduinoUI::LED(grnPin);
-  bluLED = new ArduinoUI::LED(bluPin);
-}
-void RGBLED::setColor(unsigned char newRedIntensity, unsigned char newGrnIntensity, unsigned char newBluIntensity) {
-  redLED->setIntensity(newRedIntensity);
-  grnLED->setIntensity(newGrnIntensity);
-  bluLED->setIntensity(newBluIntensity);
-}
-void RGBLED::begin() {
-  redLED->begin();
-  grnLED->begin();
-  bluLED->begin();
-}
-void RGBLED::on() {
-  redLED->on();
-  grnLED->on();
-  bluLED->on();
-}
+	RGBLED::RGBLED(int redPin, int grnPin, int bluPin) {
+		redLED = new ArduinoUI::LED(redPin);
+		grnLED = new ArduinoUI::LED(grnPin);
+		bluLED = new ArduinoUI::LED(bluPin);
+		RGBLEDstate = OFF;
+		// Timing variables
+		currentTime = millis();
+		lastTime = currentTime;
+		blinkTime = 1000;
+	}
+	void RGBLED::setColor(unsigned char newRedIntensity,
+			unsigned char newGrnIntensity, unsigned char newBluIntensity) {
+		redLED->setIntensity(newRedIntensity);
+		grnLED->setIntensity(newGrnIntensity);
+		bluLED->setIntensity(newBluIntensity);
+	}
+	void RGBLED::begin() {
+		redLED->begin();
+		grnLED->begin();
+		bluLED->begin();
+	}
+	void RGBLED::on() {
+		redLED->on();
+		grnLED->on();
+		bluLED->on();
+		RGBLED = ON;
+	}
 
-void RGBLED::off(){
-  redLED->on();
-  grnLED->on();
-  bluLED->on();
-}
+	void RGBLED::off() {
+		redLED->on();
+		grnLED->on();
+		bluLED->on();
+		RGBLED = OFF;
+	}
+
+	// These are intended to be called in the loop
+	void RGBLED::blink(unsigned long newBlinkTime) {
+		blinkTime = newBlinkTime;
+		currentTime = millis();
+		if (currentTime - lastTime > blinkTime) {
+			toggle();
+			lastTime = currentTime;
+		}
+	}
+
+	void RGBLED::blink() {
+		currentTime = millis();
+		if (currentTime - lastTime > blinkTime) {
+			toggle();
+			lastTime = currentTime;
+		}
+	}
+
+	void RGBLED::toggle() {
+		// If LEDstate is on, then turn off
+		if (RGBLED) {
+			off();
+		}
+		// Else turn on
+		else {
+			on();
+		}
+	}
 }
--- ui/ChipophoneUI/RGBLED.h
+++ ui/ChipophoneUI/RGBLED.h
@@ -2,18 +2,29 @@
 #include "LED.h"
 
 namespace ArduinoUI {
-class RGBLED {
-  public:
-    RGBLED(int redPin, int grnPin, int bluPin);
-    void setColor(unsigned char newRedIntensity, unsigned char newGrnIntensity, unsigned char newBluIntensity);
-    void begin();
-    void on();
-    void off();
-    ~RGBLED();
-  private:
-    ArduinoUI::LED * redLED;
-    ArduinoUI::LED * grnLED;
-    ArduinoUI::LED * bluLED;
-};
+	class RGBLED {
+		public:
+			RGBLED(int redPin, int grnPin, int bluPin);
+			void setColor(unsigned char newRedIntensity,
+					unsigned char newGrnIntensity,
+					unsigned char newBluIntensity);
+			void begin();
+			void on();
+			void off();
+			void toggle();
+			void blink();
+			void blink(unsigned long newBlinkTime);
+			~RGBLED();
+		private:
+			ArduinoUI::LED * redLED;
+			ArduinoUI::LED * grnLED;
+			ArduinoUI::LED * bluLED;
+			int RGBLEDstate;
+
+			// Blink timing variables
+			unsigned long currentTime;
+			unsigned long lastTime;
+			unsigned long blinkTime;
+	};
 
 }
