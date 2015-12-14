d844a90c7544a0211c1d51fd9bc428d8fa2edc21
Darren Hushak <dnhushak@pinnacle-recording.com>
2015-12-14 10:13:30 -0600

    Removed reference to Parameter List. Outdated

--- src/coreDevices/Device.cpp
+++ src/coreDevices/Device.cpp
@@ -8,22 +8,11 @@ namespace device {
 		// Device ID
 		devID = devIDCounter;
 		devIDCounter++;
-//		parameterList = new std::List<Parameter>();
 	}
 
 	Device * Device::clone() {
 		Device * newDevice = new Device();
 		newDevice->state = this->getState();
-		return newDevice;
-	}
-
-	Device * Device::cloneWithConnected() {
-		Device * newDevice = this->clone();
-		return newDevice;
-	}
-
-	Device * Device::cloneAndConnect() {
-		Device * newDevice = this->clone();
 		return newDevice;
 	}
 
