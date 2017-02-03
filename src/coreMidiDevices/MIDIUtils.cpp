1bb205fcbfcd9097834fa17638fb0b3c7e89ce89
dnhushak <dnhushak@pinnacle-recording.com>
2017-02-02 18:39:13 -0600

    Updated with float value

--- src/coreMidiDevices/MIDIUtils.cpp
+++ src/coreMidiDevices/MIDIUtils.cpp
@@ -8,6 +8,6 @@ namespace midi {
 
 	// Scale a MIDI message to a float
 	float scaleValue(int value, float min, float max) {
-		return (value * ((max - min) / 127) + min);
+		return (value * ((max - min) / 127.0) + min);
 	}
 }
